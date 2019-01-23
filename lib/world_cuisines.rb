c8f3a305197c734d3d9579b79bdc2c59508e7193
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-01-23 22:00:35 +0000

    first commit

---
+++ lib/world_cuisines.rb
@@ -0,0 +1,6 @@
+require "world_cuisines/version"
+
+module WorldCuisines
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/world_cuisines/version.rb
@@ -0,0 +1,3 @@
+module WorldCuisines
+  VERSION = "0.1.0"
+end
